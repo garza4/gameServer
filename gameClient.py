755e425746bda5bead35463e5ea8f8a6da735a49
bobbyg <garza4@southwestern.edu>
2019-08-03 19:48:23 -0500

    typo rip

--- gameClient.py
+++ gameClient.py
@@ -1,1 +1,14 @@
-print("client")
+import logging
+import grpc
+import gameServer_pb2
+import gameServer_pb2_grcp
+
+def run():
+    with grpc.insecure_channel("localhost:50051") as channel:
+        gameStub = gamerServer_pb2_grcp.GameServiceStub(channel)
+        response = stub.Move(gameServer_pb2.Direction(direction="north"))
+
+if __name__ = "__main__":
+    logging.basicConfig()
+    run()
+
